83f58860e069e1c8e82cb42f290b87aecac1fc70
mssanoko <lawsonjleslie@gmail.com>
2020-09-29 19:48:11 -0400

    server.js commenting

--- server.js
+++ server.js
@@ -15,10 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//Get the default connection
 var db = mongoose.connection;
 
+//Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console,"MongoDB connection error:"));
 
+// routes
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
