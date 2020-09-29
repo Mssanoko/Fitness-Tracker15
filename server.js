1ea133b6918ad9a54e6165d970f9cbfd4bca3ef0
mssanoko <lawsonjleslie@gmail.com>
2020-09-28 23:41:25 -0400

    server.js

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
+mongoose.connect(process.env.MONGODB_ATLAS_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console,"MongoDB connection error:"));
+
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App running on link http://localhost:3000`);
   });
