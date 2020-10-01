172dd1a18adabab1937b02dfb03f4871b52090ff
mssanoko <lawsonjleslie@gmail.com>
2020-09-30 23:24:25 -0400

    correction html-routes error

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,10 +5,10 @@ module.exports = (app) => {
     app.get("/", (req,res) => {
         res.sendFile(path.join(__dirname, "./public/index.html"));
     });
-    app.get("/exercice", (req,res) => {
-        res.sendFile(path.join(__dirname, "./public/exercice.html"));
+    app.get("/exercise", (req,res) => {
+        res.sendFile(path.join(__dirname, "./public/exercise.html"));
     });
     app.get("/stats", (req,res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
+        res.sendFile(path.join(__dirname, "./public/stats.html"));
     });
 }
