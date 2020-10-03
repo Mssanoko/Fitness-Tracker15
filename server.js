ccc34a466c6c34d301fabb5008e082b5cc35057c
mssanoko <lawsonjleslie@gmail.com>
2020-10-03 13:13:02 -0400

    deleted code in server js

--- server.js
+++ server.js
@@ -13,23 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
-//mongodb+srv://Mssanoko-admin:<Anoko228>@cluster0.ncnsk.mongodb.net/<workout>?retryWrites=true&w=majority
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Mssanoko-admin:Mssanoko@cluster0.ncnsk.mongodb.net/<workout>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("workouts");
-  // perform actions on the collection object
-  client.close();
-});
 mongoose.connect(process.env.MONGODB_ATLAS_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
-
-//Get the default connection
-var dataBase = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-dataBase.on("error", console.error.bind(console,"MongoDB connection error:"));
 
 // routes
 app.use(require("./routes/api-routes.js"));
